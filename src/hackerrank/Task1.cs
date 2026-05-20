072440552050b7fe520e0155d33e8db478b5e055
zharenovaganna <zharenova.ganna@e-u.edu.ua>
2026-05-20 15:51:51 +0300

    Update Task1.cs

--- src/hackerrank/Task1.cs
+++ src/hackerrank/Task1.cs
@@ -12,6 +12,7 @@ namespace Hackerrank1.hackerrank
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine(new string(' ', n - i) + new string('#', i));
+
             }
 
             Assert.Pass();
