ac44df9c2eddcedc30c26f6f32ebe059f63c7eee
zharenovaganna <zharenova.ganna@e-u.edu.ua>
2026-05-20 15:50:25 +0300

    Update Task1.cs

--- src/hackerrank/Task1.cs
+++ src/hackerrank/Task1.cs
@@ -1,1 +1,20 @@
+using NUnit.Framework;
 
+namespace Hackerrank1.hackerrank
+{
+    public class Task1
+    {
+        [Test]
+        public void Test_Staircase_6()
+        {
+            int n = 6;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string(' ', n - i) + new string('#', i));
+            }
+
+            Assert.Pass();
+        }
+    }
+}
